15a4cae0d14f62b69032d98918bd765102ae674b
CORP\Maulika.Desai <maulika.desai@powerschool.com>
2024-01-12 11:49:15 -0500

    no message

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import Forecast from "./components/Forecast";
 
 function App() {
-
 	return (
 		<div
 			className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 from-cyan-700 to-blue-700`}
